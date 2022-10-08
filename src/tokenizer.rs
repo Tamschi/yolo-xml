f8ff71f55932d0a317af0d0c6841eeec7d7cf18f
Tamme Schichler <tamme@schichler.dev>
2022-10-08 20:23:56 +0200

    Fixes and progress

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,7 +3,7 @@ use core::mem::{ManuallyDrop, MaybeUninit};
 use std::{ops::Deref, ptr::addr_of};
 
 type NextFn = for<'a> fn(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a>;
-type NextFnR<'a> = Result<Result<Next<'a>, Error>, OutOfBoundsError>;
+type NextFnR<'a> = Result<Next<'a>, OutOfBoundsError>;
 enum Next<'a> {
 	Exit(RetVal),
 	Call(u8, NextFn),
@@ -15,6 +15,7 @@ use Next::*;
 enum RetVal {
 	Success,
 	Failure,
+	Error(Error),
 }
 use tap::Pipe;
 use RetVal::*;
@@ -22,20 +23,22 @@ use RetVal::*;
 /// [1]
 fn document<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 	match (state, ret_val) {
+		(_, Error(error)) => Exit(Error(error)),
 		(0, _) => Call(1, prolog),
 		(1, Success) => Call(2, element),
 		(2 | 3, Success) => Call(3, Misc),
 		(3, Failure) => Exit(Success),
-		(1 | 2, Failure) => Exit(Failure),
+		(1, Failure) => Exit(Error(Error::ExpectedProlog)),
+		(2, Failure) => Exit(Error(Error::ExpectedElement)),
 		_ => unreachable!(),
 	}
-	.pipe(Ok)
 	.pipe(Ok)
 }
 
 /// [3]
 fn S<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 	match (state, ret_val) {
+		(_, Error(error)) => Exit(Error(error)),
 		(0, _) => match buffer
 			.shift_known_array(&[0x20])
 			.transpose()
@@ -58,35 +61,37 @@ fn S<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 			Some(_) => Continue(1),
 			None => Exit(Success),
 		},
+		_ => unreachable!(),
 	}
-	.pipe(Ok)
 	.pipe(Ok)
 }
 
 /// [22]
 fn prolog<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 	match (state, ret_val) {
+		(_, Error(error)) => Exit(Error(error)),
 		(0, _) => Call(1, XMLDecl),
 		(1 | 2, Success) => Call(2, Misc),
 		(2, Failure) => Call(3, doctypedecl),
 		(3 | 4, Success) => Call(4, Misc),
 		(3 | 4, Failure) => Exit(Success),
-		(1, Failure) => Exit(Failure),
+		(1, Failure) => unreachable!("should downgrade"),
 		_ => unreachable!(),
 	}
-	.pipe(Ok)
 	.pipe(Ok)
 }
 
 /// [23]
 fn XMLDecl<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 	match (state, ret_val) {
+		(_, Error(error)) => Exit(Error(error)),
 		(0, _) => match buffer.shift_known_array(b"<?xml")? {
 			Some(_) => Continue(1),
 			None => Yield(0, Event::DowngradeFrom1_1),
 		},
 		(1, _) => Call(2, VersionInfo),
 		(2, Success) => Call(3, EncodingDecl),
+		(2, Failure) => Exit(Error(Error::Expected24VersionInfo)),
 		(3, _) => Call(4, SDDecl),
 		(4, _) => Call(5, S),
 		(5, _) => match buffer.shift_known_array(b"?>")? {
@@ -94,72 +99,74 @@ fn XMLDecl<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a
 			None => Failure,
 		}
 		.pipe(Exit),
-		(2, Failure) => Exit(Failure),
 		_ => unreachable!(),
 	}
-	.pipe(Ok)
 	.pipe(Ok)
 }
 
 /// [24]
 fn VersionInfo<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 	match (state, ret_val) {
+		(_, Error(error)) => Exit(Error(error)),
 		(0, _) => Call(1, S),
 		(1, Success) => match buffer.shift_known_array(b"version")? {
 			Some(_) => Continue(2),
-			None => Exit(Failure),
+			None => Exit(Error(Error::ExpectedLiteral(b"version"))),
 		},
+		(1, Failure) => Exit(Failure),
 		(2, _) => Call(3, Eq),
 		(3, Success) => match buffer.shift_known_array(b"'")? {
 			Some(_) => Continue(4),
 			None => match buffer.shift_known_array(b"\"")? {
 				Some(_) => Continue(6),
-				None => Exit(Failure),
+				None => Exit(Error(Error::ExpectedQuote)),
 			},
 		},
+		(3, Failure) => unreachable!("`Eq` shouldn't fail."),
 		(4, _) => Call(5, VersionNum),
 		(5, Success) => match buffer.shift_known_array(b"'")? {
 			Some(_) => Exit(Success),
-			None => Exit(Failure),
+			None => Exit(Error(Error::ExpectedLiteral(b"'"))),
 		},
 		(6, _) => Call(7, VersionNum),
 		(7, Success) => match buffer.shift_known_array(b"\"")? {
 			Some(_) => Exit(Success),
-			None => Exit(Failure),
+			None => Exit(Error(Error::ExpectedLiteral(b"\""))),
 		},
-		(1 | 3 | 5 | 7, Failure) => Exit(Failure),
+		(5 | 7, Failure) => unreachable!("should downgrade"),
 		_ => unreachable!(),
 	}
-	.pipe(Ok)
 	.pipe(Ok)
 }
 
 /// [25]
+///
+/// Never returns `Ok(Exit(Failure))`.
 fn Eq<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 	match (state, ret_val) {
+		(_, Error(error)) => Exit(Error(error)),
 		(0, _) => Call(1, S),
 		(1, _) => match buffer.shift_known_array(b"=")? {
 			Some(_) => Continue(2),
-			None => Exit(Failure),
+			None => Exit(Error(Error::ExpectedLiteral(b"="))),
 		},
 		(2, _) => Call(3, S),
 		(3, _) => Exit(Success),
 		_ => unreachable!(),
 	}
 	.pipe(Ok)
-	.pipe(Ok)
 }
 
 /// [26]
 fn VersionNum<'a>(buffer: &'a mut StrBuf, state: u8, ret_val: RetVal) -> NextFnR<'a> {
 	match (state, ret_val) {
+		(_, Error(error)) => Exit(Error(error)),
 		(0, _) => match buffer.shift_known_array(b"1.1")? {
 			Some(version) => Yield(1, Event::Version(version)),
 			None => Yield(0, Event::DowngradeFrom1_1),
-		}
-		.pipe(Ok)
-		.pipe(Ok),
+		},
 	}
+	.pipe(Ok)
 }
 
 #[non_exhaustive]
@@ -168,4 +175,11 @@ enum Event<'a> {
 	Version(&'a mut [u8]),
 }
 
-enum Error {}
+enum Error {
+	ExpectedElement,
+	ExpectedProlog,
+	ExpectedWhitespace,
+	ExpectedLiteral(&'static [u8]),
+	ExpectedQuote,
+	Expected24VersionInfo,
+}
